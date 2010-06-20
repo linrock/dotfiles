cdb3ba73fa4a571850e4bdd631086782cba02b2a
Linmiao Xu <linmiao.xu@gmail.com>
2010-06-20 01:45:29 -0700

    Added more options to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -6,7 +6,6 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Run (spawnPipe)
@@ -26,13 +25,13 @@ myTopStatusBar  = "dzen2 -x '0' -y '0' -h '24' -w '1920' -ta 'l' -fg '#FFFFFF' -
 myBtmStatusBar  = "conky -c ~/.conky_bottom_dzen | dzen2 -x '0' -y '1056' -h '24' -w '1920' -ta 'c' -bg '#161616' -fg '#FFFFFF' -fn " ++ myFont
 myBitmapsDir    = "/usr/share/dzen"
 myWorkspaces    = 
-    [ "1:term1"
-    , "2:term2"
-    , "3:web"
-    , "4:music"
-    , "5:misc1"
-    , "6:misc2"
-    , "7:vbox"
+    [ "1:term"
+    , "2:web"
+    , "3:dev"
+    , "4:media"
+    , "5:fire"
+    , "6:misc"
+    , "7:win"
     , "8:comm"
     , "9:logs"
     ]
@@ -62,6 +61,8 @@ myKeyz =
     , ((0, 0x1008ff12       ), spawn "amixer set Master toggle")  -- mute volume
     , ((0, 0x1008ff11       ), spawn "amixer set Master 5-")      -- decrease volume
     , ((0, 0x1008ff13       ), spawn "amixer set Master 5+")      -- increase volume
+    
+    , ((mod4Mask, xK_q      ), spawn "xmonad --recompile; killall dzen2; xmonad --restart")
     ]
 
 -- Hooks {{{
@@ -86,21 +87,25 @@ myLogHook h = dynamicLogWithPP $ defaultPP
     , ppOutput           = hPutStrLn h
     }
 
-myLayoutHook = avoidStruts $ smartBorders $ (ThreeCol 1 (3/100) (1/2) ||| Grid ||| Dishes 2 (1/6) ||| Full)
+myLayoutHook = avoidStruts $ smartBorders $ (ThreeCol 1 (3/100) (1/3) ||| Grid ||| Dishes 2 (1/6) ||| Full)
 
 myManageHook :: ManageHook
 myManageHook = (composeAll . concat $
     [ [isFullscreen        --> doF W.focusDown <+> doFullFloat     ]    -- allows focusing on other windows without leaving FS
-    , [className     =? c  --> doShift "3:web1"  |  c  <- myWebs   ]
+    , [className     =? c  --> doShift "2:web"   |  c  <- myWebs   ]
+    , [className     =? c  --> doShift "4:media" |  c  <- myMedia  ]
+    , [className     =? c  --> doShift "5:fire"  |  c  <- myFires  ]
     , [className     =? c  --> doCenterFloat     |  c  <- myFloats ]
-    , [name          =? n  --> doCenterFloat     |  n  <- myNames  ]
+    , [wmName        =? n  --> doCenterFloat     |  n  <- myNames  ]
     ])
     where
-        role      = stringProperty "WM_WINDOW_ROLE"
-        name      = stringProperty "WM_NAME"
+        wmRole    = stringProperty "WM_WINDOW_ROLE"
+        wmName    = stringProperty "WM_NAME"
 
         myFloats  = ["VirtualBox", "MPlayer", "Save As..."]
-        myWebs    = ["Firefox", "Chrome"]
-        myNames   = ["Firefox Preferences", "Chromium Options", "bashrun"]
+        myWebs    = ["Chrome"]
+        myMedia   = ["MPlayer"]
+        myFires   = ["Firefox", "Minefield"]
+        myNames   = ["Firefox Preferences", "Add-ons", "Chromium Options", "bashrun"]
 -- }}}
 
