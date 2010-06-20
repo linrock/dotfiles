2468cc3d32969aa1cb33fcb384311b17abe87bc2
Linmiao Xu <linmiao.xu@gmail.com>
2010-06-20 16:39:26 -0700

    More xmonad and conky tweaks.

--- xmonad.hs
+++ xmonad.hs
@@ -19,6 +19,7 @@ import qualified XMonad.StackSet as W
 
 import System.IO
 
+
 myTerminal      = "urxvtc"
 myFont          = "-*-terminus-medium-*-*-*-12-120-75-75-*-*-iso8859-*"
 myTopStatusBar  = "dzen2 -x '0' -y '0' -h '24' -w '1920' -ta 'l' -fg '#FFFFFF' -bg '#161616' -fn " ++ myFont
@@ -28,8 +29,8 @@ myWorkspaces    =
     [ "1:term"
     , "2:web"
     , "3:dev"
-    , "4:media"
-    , "5:fire"
+    , "4:fire"
+    , "5:media"
     , "6:misc"
     , "7:win"
     , "8:comm"
@@ -51,19 +52,17 @@ main = do
         , workspaces            = myWorkspaces
         , normalBorderColor     = "#707070"
         , focusedBorderColor    = "#ffff00"
-        } `additionalKeys`        myKeyz
+        } `additionalKeys`
+            [ ((mod1Mask, xK_F2     ), spawn "bashrun")         -- intuitive Alt-F2 = run window
+            , ((mod1Mask, xK_F4     ), kill)                    -- intuitive Alt-F4 = quit
+            , ((mod1Mask, xK_Tab    ), windows W.focusDown)     -- intuitive Alt-Tab = next window
 
-myKeyz = 
-    [ ((mod1Mask, xK_F2     ), spawn "bashrun")         -- intuitive Alt-F2 = run window
-    , ((mod1Mask, xK_F4     ), kill)                    -- intuitive Alt-F4 = quit
-    , ((mod1Mask, xK_Tab    ), windows W.focusDown)     -- intuitive Alt-Tab = next window
-
-    , ((0, 0x1008ff12       ), spawn "amixer set Master toggle")  -- mute volume
-    , ((0, 0x1008ff11       ), spawn "amixer set Master 5-")      -- decrease volume
-    , ((0, 0x1008ff13       ), spawn "amixer set Master 5+")      -- increase volume
-    
-    , ((mod4Mask, xK_q      ), spawn "xmonad --recompile; killall dzen2; xmonad --restart")
-    ]
+            , ((0, 0x1008ff12       ), spawn "amixer set Master toggle")  -- mute volume
+            , ((0, 0x1008ff11       ), spawn "amixer set Master 5-")      -- decrease volume
+            , ((0, 0x1008ff13       ), spawn "amixer set Master 5+")      -- increase volume
+            
+            , ((mod4Mask, xK_q      ), spawn "xmonad --recompile; killall dzen2; xmonad --restart")
+            ]
 
 -- Hooks {{{
 myLogHook :: Handle -> X ()
@@ -88,13 +87,12 @@ myLogHook h = dynamicLogWithPP $ defaultPP
     }
 
 myLayoutHook = avoidStruts $ smartBorders $ (ThreeCol 1 (3/100) (1/3) ||| Grid ||| Dishes 2 (1/6) ||| Full)
-
 myManageHook :: ManageHook
 myManageHook = (composeAll . concat $
     [ [isFullscreen        --> doF W.focusDown <+> doFullFloat     ]    -- allows focusing on other windows without leaving FS
     , [className     =? c  --> doShift "2:web"   |  c  <- myWebs   ]
-    , [className     =? c  --> doShift "4:media" |  c  <- myMedia  ]
-    , [className     =? c  --> doShift "5:fire"  |  c  <- myFires  ]
+    , [className     =? c  --> doShift "4:fire"  |  c  <- myFires  ]
+    , [className     =? c  --> doShift "5:media" |  c  <- myMedia  ]
     , [className     =? c  --> doCenterFloat     |  c  <- myFloats ]
     , [wmName        =? n  --> doCenterFloat     |  n  <- myNames  ]
     ])
@@ -102,10 +100,10 @@ myManageHook = (composeAll . concat $
         wmRole    = stringProperty "WM_WINDOW_ROLE"
         wmName    = stringProperty "WM_NAME"
 
-        myFloats  = ["VirtualBox", "MPlayer", "Save As..."]
         myWebs    = ["Chrome"]
-        myMedia   = ["MPlayer"]
         myFires   = ["Firefox", "Minefield"]
+        myMedia   = ["MPlayer"]
+        myFloats  = ["VirtualBox", "MPlayer", "Save As..."]
         myNames   = ["Firefox Preferences", "Add-ons", "Chromium Options", "bashrun"]
 -- }}}
 
