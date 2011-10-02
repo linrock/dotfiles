932b473d361a8a32a52fce48591704186e8d09da
Linmiao Xu <linmiao.xu@gmail.com>
2011-10-01 20:38:23 -0700

    Xmonad.hs update with wmname fix for matlab

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,6 +11,8 @@ import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
 
 import XMonad.Util.Themes
 import XMonad.Util.EZConfig
@@ -18,31 +20,24 @@ import XMonad.Util.Run (spawnPipe)
 
 import XMonad.Layout.Grid
 import XMonad.Layout.Dishes
+import XMonad.Layout.Minimize
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.TwoPane
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
 import XMonad.Layout.MultiToggle
+import XMonad.Layout.SimpleFloat
 
 import qualified XMonad.StackSet as W
 
 -- Variables {{{
 myBarBgColor      = "#000000"
-myTerminal        = "urxvtc"
+myTerminal        = "urxvt"
 myFont            = "-*-terminus-medium-*-*-*-12-120-75-75-*-*-iso8859-*"
-myXmonadStatusBar = "dzen2 -x '0' -y '776' -h '24' -w '1280' -ta 'l' -fg '#FFFFFF' -bg '" ++ myBarBgColor ++ "' -fn " ++ myFont
+myXmonadStatusBar = "dzen2 -x '0' -y '1056' -h '24' -w '1920' -ta 'l' -fg '#FFFFFF' -bg '" ++ myBarBgColor ++ "' -fn " ++ myFont
 mySysStatusBar    = "conky -c ~/.conkyrc | dzen2 -x '0' -y '0' -h '24' -w '1920' -ta 'c' -bg '" ++ myBarBgColor ++ "' -fg '#FFFFFF' -fn " ++ myFont
-myWorkspaces      = 
-    [ "1:term"
-    , "2:web"
-    , "3:dev"
-    , "4:ssh"
-    , "5:vbox"
-    , "6:misc"
-    , "7:media"
-    , "8:comm"
-    , "9:logs"
-    ]
+myWorkspaces      = [ ".1:",":2.",".3:",":4.",".5:",":6.",".7:",":8.",".9:"]
 -- }}}
 
 -- Prompt {{{
@@ -71,6 +66,7 @@ main = do
         , layoutHook            = myLayoutHook
         , manageHook            = myManageHook
         , logHook               = myLogHook dzenTopBar
+        , startupHook           = ewmhDesktopsStartup >> setWMName "LG3D"
         , modMask               = mod4Mask      -- set the mod key to the windows key
         , borderWidth           = 2
         , focusFollowsMouse     = False
@@ -78,13 +74,15 @@ main = do
         , normalBorderColor     = "#707070"
         , focusedBorderColor    = "#fbb917"
         } `additionalKeys`
-            -- [ ((mod1Mask, xK_F2   ), spawn "bashrun")         -- Alt-F2 = run window
-            [ ((mod1Mask, xK_F2   ), runOrRaisePrompt myXPConfig)
-            , ((mod1Mask, xK_F4   ), kill)                          -- Alt-F4 = quit
-            , ((mod1Mask, xK_Tab  ), windows W.focusDown)           -- Alt-Tab = next window
-            , ((mod4Mask, xK_x    ), sendMessage $ Toggle REFLECTX)
-            , ((mod4Mask, xK_y    ), sendMessage $ Toggle REFLECTY)
-            , ((mod4Mask, xK_q    ), spawn "xmonad --recompile; killall dzen2; xmonad --restart")
+            -- , ((mod1Mask, xK_Tab  ), windows W.focusDown)           -- Alt-Tab = next window
+            -- [ ((mod1Mask, xK_F2                 ), spawn "bashrun2")
+            [ ((mod1Mask, xK_F2                 ), runOrRaisePrompt myXPConfig)
+            , ((mod1Mask, xK_F4                 ), kill)                          -- Alt-F4 = quit
+            -- , ((mod4Mask, xK_u                  ), withFocused (\f -> sendMessage (MinimizeWin f)))
+            -- , ((mod4Mask .|. shiftMask, xK_u    ), sendMessage RestoreNextMinimizedWin)
+            , ((mod4Mask, xK_x                  ), sendMessage $ Toggle REFLECTX)
+            , ((mod4Mask, xK_y                  ), sendMessage $ Toggle REFLECTY)
+            , ((mod4Mask, xK_q                  ), spawn "xmonad --recompile && killall dzen2 && xmonad --restart")
             ]
 -- }}}
 
@@ -94,7 +92,7 @@ myLogHook h = dynamicLogWithPP $ dzenPP
     { ppCurrent          = dzenColor "#ffcc00" "black" . pad
     , ppVisible          = dzenColor "#f4f4f4" "black" . pad
     , ppHidden           = dzenColor "#707070" "black" . pad
-    , ppHiddenNoWindows  = dzenColor "#303030" "black" . pad
+    , ppHiddenNoWindows  = dzenColor "black"   "black" . pad
     , ppUrgent           = dzenColor "black"   "orange" . pad
     , ppWsSep            = " "
     , ppSep              = "  |  "
@@ -103,24 +101,26 @@ myLogHook h = dynamicLogWithPP $ dzenPP
     , ppOutput           = hPutStrLn h
     }
 
+-- myLayoutHook = avoidStruts $ smartBorders $ minimize $
 myLayoutHook = avoidStruts $ smartBorders $
     mkToggle (single REFLECTX) $ mkToggle (single REFLECTY) $
-        onWorkspace     "1:term"    (Grid ||| Dishes 2 (1/6)) $
-        onWorkspace     "2:web"     (noBorders Grid ||| noBorders Full) $
-        onWorkspace     "3:dev"     (Grid ||| Dishes 2 (1/6) ||| noBorders Full) $
-        onWorkspace     "4:ssh"     (Grid ||| Dishes 2 (1/6)) $
-        onWorkspace     "5:vbox"    (noBorders Full) $
-        onWorkspace     "7:media"   (noBorders Full ||| Grid) $
-        (noBorders Full ||| Grid ||| ThreeCol 1 (3/100) (1/3) ||| Dishes 2 (1/6))
+        onWorkspace     ".1:"    (Grid ||| ThreeCol 1 (3/100) (1/3) ||| TwoPane (3/100) (1/2) ||| Full) $
+        onWorkspace     ":2."    (noBorders Full ||| Grid ||| ThreeCol 1 (3/100) (1/3)) $
+        onWorkspace     ".3:"    (Grid ||| ThreeCol 1 (3/100) (1/3) ||| noBorders Full) $
+        onWorkspace     ":4."    (Grid ||| ThreeCol 1 (3/100) (1/3) ||| Full) $
+        onWorkspace     ".5:"    (Grid ||| noBorders Full) $
+        onWorkspace     ":6:"    simpleFloat $
+        onWorkspace     ".7:"    (noBorders Full ||| Grid) $
+        (noBorders Full ||| Grid ||| ThreeCol 1 (3/100) (1/3))
 
 myManageHook :: ManageHook
 myManageHook = (composeAll . concat $
     -- [[ isFullscreen        --> doF W.focusDown <+> doFullFloat       ]
     [[ isFullscreen        --> doFullFloat                           ]
     ,[ className     =? i  --> doIgnore          |  i  <- myIgnores  ]
-    ,[ className     =? c  --> doShift "2:web"   |  c  <- myBrowsers ]
-    ,[ className     =? c  --> doShift "5:vbox"  |  c  <- myVMs      ]
-    ,[ className     =? c  --> doShift "8:comm"  |  c  <- myComms    ]
+    ,[ className     =? c  --> doShift ":2."     |  c  <- myBrowsers ]
+    ,[ className     =? c  --> doShift ".5:"     |  c  <- myVMs      ]
+    ,[ className     =? c  --> doShift ":8."     |  c  <- myComms    ]
     ,[ className     =? c  --> doCenterFloat     |  c  <- myFloats   ]
     ,[ wmName        =? n  --> doCenterFloat     |  n  <- myNames    ]
     ])
@@ -129,10 +129,10 @@ myManageHook = (composeAll . concat $
         wmName      = stringProperty "WM_NAME"
 
         myIgnores   = ["trayer"]
-        myBrowsers  = ["Firefox", "Minefield", "Namoroka", "Chrome"]
+        myBrowsers  = ["Firefox", "Chrome"]
         myVMs       = ["VirtualBox"]
-        myComms     = ["Pidgin"]
-        myFloats    = ["Gtick", "Nitrogen", "feh", "MPlayer", "Pidgin", "Save As...", "Mirage"]
-        myNames     = ["Namoroka Preferences", "Add-ons", "Downloads", "Manage Proxies", "Proxy Info", "Chromium Preferences"]
+        myComms     = ["Lanikai", "Pidgin"]
+        myFloats    = ["Nitrogen", "MPlayer", "Pidgin", "Save As...", "Mirage", "VirtualBox"]
+        myNames     = ["Firefox Preferences", "Downloads", "Chromium Preferences", "Firebug"]
 -- }}}
 
